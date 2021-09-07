e5d258382960b7cc6503cf572fd640811c0afc90
Jeffersonziig <86735537+Jeffersonziig@users.noreply.github.com>
2021-09-07 16:35:38 -0300

    Add files via upload

---
+++ Series.dio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Series.dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Series meuObjeto = new Serie();
+            Series meuObjeto = new
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
